7018f01246978efb02c99f0218a65c03ca903080
MD RAIFUL ISLAM <150923484+rafimuna@users.noreply.github.com>
2024-10-03 09:17:18 +0000

    array join

--- 01_Basics/05_array1.js
+++ 01_Basics/05_array1.js
@@ -1,5 +1,10 @@
 const cars = ['KADU','MADU','CHADU','HAGU'];
 let size = cars.length;
 console.log(size);//array size
-console.log(car[2]);//array index
+console.log(cars[2]);//array index
+// array join 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let sum1 = fruits.join(" * ");
+console.log(sum1);
+
 
